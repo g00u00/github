ea5d22dfd9aab8ec3e8711efbe7f9fca7f303efc
g00u00 <g00u00@outlook.com>
2022-08-22 15:09:00 +0300

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -10,22 +10,34 @@ git config user.name "g00u00";
 git config user.email "g00u00@outlook.com";
 git branch -M main;
 git branch;
+
 echo "# a" >> README.tmp
+git status;
+git add .;
+git status;
+git status --ignored;
+git commit -m'add README.tmp';
+git status;
+git status --ignored;
+
 #git remote add origin git@github.com:g00u00/github.git;
 #git config --list;
 git pull origin main;
 git status;
 git status --ignored;
 ls -lAF;
-git log -n2 --oneline --graph --all;
+
+rm README.tmp
 git status;
 git add .;
 git status;
 git status --ignored;
-git commit -m'rm pull.sh';
-#git log -n2 --oneline --graph --all;
+git commit -m'rm README.tmp';
 git status;
 git status --ignored;
+
+git log -n2 --oneline --graph --all;
+
 echo "git reflog --all"
 git reflog --all
 #git cat-file -p 546fcd1
