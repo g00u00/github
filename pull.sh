9419e286b7e89174c6ecf9e8f0815f68e63fd33a
g00u00 <g00u00@outlook.com>
2022-08-22 15:20:50 +0300

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -11,7 +11,15 @@ git config user.email "g00u00@outlook.com";
 git branch -M main;
 git branch;
 
-echo "# a" >> README.tmp
+#git remote add origin git@github.com:g00u00/github.git;
+#git config --list;
+git pull origin main;
+git status;
+git status --ignored;
+ls -lAF;
+git log -n4 --oneline --graph --all;
+
+echo "# a" >> README.tmp;
 git status;
 git add .;
 git status;
@@ -20,12 +28,6 @@ git commit -m'add README.tmp';
 git status;
 git status --ignored;
 
-#git remote add origin git@github.com:g00u00/github.git;
-#git config --list;
-git pull origin main;
-git status;
-git status --ignored;
-ls -lAF;
 
 rm README.tmp
 git status;
